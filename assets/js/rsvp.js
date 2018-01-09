917551431f6af1b2b65eb7f072dd8cd45ea437e6
Ricardo Henrique Gracini Guiraldelli <ricardo.guiraldelli@gmail.com>
2018-01-09 22:05:20 +0100

    Fooling around with JS.

--- assets/js/rsvp.js
+++ assets/js/rsvp.js
@@ -41,7 +41,7 @@ $('#submit-form').on('click', function(e) {
   var jqxhr = $.ajax(
     { 
       url: url
-    , method: "GET"
+    , method: "POST"
     , dataType: "json"
     , data: newJson
     })
